6a4c1f36aca542a9a36136b437fabf670662e3d4
Buiu <buiuzinsdc@gmail.com>
2023-06-21 12:37:07 -0300

    Git

--- conexao.php
+++ conexao.php
@@ -2,10 +2,10 @@
 session_start();
 $base = 'http://localhost:8765/';
 
-$db_name = 'shoppobre';
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
+$db_name = 'heroku_db9506974be2de2';
+$db_host = 'us-cdbr-east-06.cleardb.net';
+$db_user = 'bab35029f77dfd';
+$db_pass = 'a6448ac5';
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 ?>
